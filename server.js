64d31cf1ca284570fe281d575acd57c8f13a62bc
Glitch (telegrime) <none>
2021-02-03 13:01:10 +0000

    Add Telegraf.js ^3

--- server.js
+++ server.js
@@ -1,66 +1,42 @@
-const fs = require("fs");
-let flypy = fs.readFileSync("flypy.txt", "utf-8");
-flypy = flypy.split("\n"); //.splice(1, 55);
-flypy = flypy.map(x => {
-  x = x.split(",");
-  let a = x[0].split("=");
-  a[1] = Number(a[1]);
-  return a.concat(x[1]);
+var crc32 = function(r) {
+  for (var a, o = [], c = 0; c < 256; c++) {
+    a = c;
+    for (var f = 0; f < 8; f++) a = 1 & a ? 3988292384 ^ (a >>> 1) : a >>> 1;
+    o[c] = a;
+  }
+  for (var n = -1, t = 0; t < r.length; t++)
+    n = (n >>> 8) ^ o[255 & (n ^ r.charCodeAt(t))];
+  return ((-1 ^ n) >>> 0).toString(10);
+};
+
+const flypy = require("./flypy");
+
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.TELEGRIME);
+
+bot.start(ctx => ctx.reply("Welcome"));
+bot.help(ctx => ctx.reply("Send me a sticker"));
+bot.on("sticker", ctx => ctx.reply("👍"));
+bot.hears("hi", ctx => ctx.reply("Hey there"));
+
+bot.on("inline_query", async ({ inlineQuery, answerInlineQuery }) => {
+  const q = inlineQuery.query;
+  console.log(q)
+  if (!q) return
+  let res = flypy.up(q);
+  var results = [
+    {
+      type: "article",
+      id: crc32(res),
+      title: "FLYPY",
+      description: res,
+      input_message_content: {
+        message_text: res
+      }
+    }
+  ];
+  return answerInlineQuery(results);
 });
 
-let input = "xmzdu yyxu eewdd ksged,soyijiuiu do daf l ksge ye u m wftid.";
-input = input + " ";
-let chars = input.split("");
-let stack = "";
-let output = [];
-function lookup(word, w) {
-  if (!w) {
-    w = 1;
-  }
-  for (let x of flypy) {
-    if (x[0] == word && x[1] == w) return x;
-  }
-  
-  return ["", 0, ""];
-}
-
-function dealWithStack() {
-  if (!stack) return;
-  output.push(lookup(stack)[2]);
-  stack = "";
-}
-const punctuations = (",./?<>~!'[{]}" + '"\\').split("");
-let allowMoreSpaces = true;
-let i = 100;
-while (i > 0) {
-  i = i - 1;
-  var char = chars.shift();
-  //if (char) console.log(char);
-  //console.log(char);
-  if (char === " " && allowMoreSpaces) {
-    stack ? dealWithStack() : output.push(" ");
-  } else if (stack.length == 4) {
-    dealWithStack();
-    stack = char;
-  } else if (char === " " && !allowMoreSpaces) {
-    stack ? dealWithStack() : output.push(" ");
-  } else if (/[a-z]/.test(char)) {
-    stack = stack + char;
-  } else if (/[A-Z]/.test(char)) {
-    stack ? dealWithStack() : output.push(char);
-  } else if (punctuations.indexOf(char) != -1) {
-    switch (char) {
-      case ".":
-        dealWithStack();
-        output.push("。");
-        break;
-      case ",":
-        dealWithStack();
-        output.push("，");
-        break;
-    }
-    //if (/(\.|,|\/)/.test(char)){
-  }
-}
-
-console.log(output.join(""));
+bot.launch();
